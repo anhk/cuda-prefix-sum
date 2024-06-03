bd00bb72f17956c8d3189f63d459290de46d8684
anhongkui <anhongkui@jd.com>
2024-06-03 14:19:17 +0000

    __device__ && __host__

--- by_cuda.cu
+++ by_cuda.cu
@@ -2,21 +2,8 @@
 #include <stdio.h>
 
 #define BLOCK_SIZE 10
-uint32_t power_of_2(uint32_t val)
-{
-	if ((val & (val - 1)) == 0) {
-		return val;
-	}
-	uint32_t andv = 0x80000000;
 
-	while ((andv & val) == 0) {
-		andv >>= 1;
-	}
-
-	return andv << 1;
-}
-
-__device__ uint32_t power_of_2_d(uint32_t val)
+__host__ __device__ uint32_t power_of_2(uint32_t val)
 {
 	if ((val & (val - 1)) == 0) {
 		return val;
@@ -46,7 +33,7 @@ __global__ void work_efficient_scan_kernel(int32_t* X, ssize_t InputSize,
 		__syncthreads();
 
 		if (i < InputSize && ((i + 1) % stride) == 0 &&
-		    ((i + 1) % (stride * 2)) != 0 && power_of_2_d(i + 1) != i + 1) {
+		    ((i + 1) % (stride * 2)) != 0 && power_of_2(i + 1) != i + 1) {
 
 			int32_t sum = 0;
 			ssize_t pos = 0;
